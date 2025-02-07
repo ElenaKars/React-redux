563fd492d7a8e07e424aae4e14f7f437f71e6d6f
ElenaKars <elena_baldina@yahoo.com>
2025-02-07 13:11:25 +0100

    lesson17

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,25 +1,25 @@
-import { useState } from "react";
+import { counterActions, counterSelectors } from "store/redux/counter/counterSlice";
 import Button from "../Button/Button";
 import { CounterWrapper, ButtonsWrapper, ResultContainer } from "./styles";
+import { useAppDispatch, useAppSelector } from "store/hooks";
 
 function Counter() {
-  const [counter, setCounter] = useState<number>(0);
-
-  const onPlusClick = (): void => {
-    setCounter((prevState) => prevState + 1);
+  const counter = useAppSelector(counterSelectors.counterValue);
+  const dispatch = useAppDispatch();
+  const onMinus = () => {
+    dispatch(counterActions.minus());
   };
-
-  const onMinusClick = (): void => {
-    setCounter((prevState) => (prevState > 0 ? prevState - 1 : prevState));
+  const onPlus = () => {
+    dispatch(counterActions.plus());
   };
   return (
     <CounterWrapper>
       <ButtonsWrapper>
-        <Button name="-" onClick={onMinusClick} type="button" />
+        <Button name="-" onClick={onMinus} type="button" />
       </ButtonsWrapper>
       <ResultContainer>{counter}</ResultContainer>
       <ButtonsWrapper>
-        <Button name="+" onClick={onPlusClick} type="button" />
+        <Button name="+" onClick={onPlus} type="button" />
       </ButtonsWrapper>
     </CounterWrapper>
   );
